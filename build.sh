7c00731814ebf114b7723b3eaf2241c35a2afc36
Chris Gao <chris.gao@laserfiche.com>
2017-08-17 17:07:55 -0700

    Split run command into run_parameters.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@ docker stop $(docker ps -a -q)
 # build docker container for predictionio 
 docker build -t pio .
 # run docker container
-docker run -it -p 7070:7070 -p 8080:8080 -p 9000:9000 pio bash
-# start eventserver
-# pio eventserver &
+
---
+++ run_parameters.sh
@@ -0,0 +1,1 @@
+docker run -it -p 7070:7070 -p 8080:8080 -p 9000:9000 pio bash
